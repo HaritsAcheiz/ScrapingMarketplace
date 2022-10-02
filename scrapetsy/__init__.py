251339529654345817230672d213133ed6a71bb5
HARITS <harits.muhammad.only@gmail.com>
2022-10-03 04:01:42 +0700

    success get url 1 page

--- scrapetsy/__init__.py
+++ scrapetsy/__init__.py
@@ -4,45 +4,95 @@ all function and method for scrapetsy
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
 class Scrapetsy:
     # define parent class variable
-    def __init__(self, url,result):
-        self.url = url
+    def __init__(self, scheme, host, filename, params, result):
+        self.scheme = scheme
+        self.host = host
+        self.params = params
+        self.filename = filename
         self.header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'}
         self.result = result
     # define parent class function
-    def extract(self):
-        self.extract()
+
+    # def get_page(self):
+    #     self.get_page()
+
+    def get_url(self):
+        self.get_url()
+
+    # def run(self):
+    #     self.get_page()
+    #     self.get_url()
 
 # Class to get popular women gift
 class WomenGift(Scrapetsy):
-    # define child class variable aka "Constructor"
+    # define variable in child class WomanGift
     def __init__(self):
         super(WomenGift,self).__init__(
-            url = 'https://www.etsy.com/search?q=gift+for+women&ref=hp_gbs&anchor_listing_id=737271222',
-            result = {'title':'Most Popular Women Gift','description':'Class to get Most Popular Women Gift','content':[]})
+            # https://www.etsy.com/search?q=gift+for+women&ref=pagination&anchor_listing_id=737271222&page=1
+            scheme = 'https', #://
+            host = 'www.etsy.com',
+            filename = '/search', #?
+            params = {'q':'gift+for+women', 'ref':'pagination', 'anchor_listing_id':'737271222', 'page':'1'},
+            result = {'title':'Most Popular Women Gift','description':'Class to get Most Popular Women Gift','content':['']})
 
-    #define child class function
-    def extract(self):
+    # define function in child class WomanGift
+
+    def get_url(self,page):
+        print('getting url...')
+        # real access to web
         try:
-            with requests.Session() as session:
-                response = session.get(self.url,headers=self.header)
+            driver = webdriver.Firefox(executable_path='C:/geckodriver-v0.31.0-win64/geckodriver.exe')
+            # url = 'https://www.etsy.com/search?q=gift+for+women&ref=pagination&anchor_listing_id=737271222&page=1'
+            url = f"{self.scheme}://{self.host}{self.filename}?q={self.params['q']}&ref={self.params['ref']}&anchor_listing_id={self.params['anchor_listing_id']}&page={page}"
+            driver.get(url)
+            time.sleep(5)
+            response = driver.page_source
+
         except:
             response = 'Invalid Format'
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            soup = soup.find('ul', {'class':'wt-grid wt-grid--block wt-pl-xs-0 tab-reorder-container'})
-            soup = soup.findAll('li', {'class':'wt-list-unstyled wt-grid__item-xs-6 wt-grid__item-md-4 wt-grid__item-lg-3 wt-order-xs-0 wt-order-md-0 wt-order-lg-0 wt-show-xs wt-show-md wt-show-lg'})
-            for i in soup:
-                datacontent = {'image':'', 'name':'', 'solditem':'', 'badge':'', 'price':'', 'promotion':'', 'shop':'', 'link':''}
-                datacontent['image'] = i.find('img')['src']
-                print(datacontent['image'])
+        soup = BeautifulSoup(response, 'html.parser')
+
+        # f = open('response.html', 'w+', encoding="utf-8")
+        # f.write(response)
+        # f.close()
+
+        # checking file response.html
+        # soup = BeautifulSoup(open('response.html', encoding = 'utf-8'),'html.parser')
+
+        soup = soup.findAll('div', {'class':'js-merch-stash-check-listing v2-listing-card wt-mr-xs-0 search-listing-card--desktop listing-card-experimental-style appears-ready'})
+        for i in soup:
+            result = i.find('a', href=True)['href']
+            print(result)
+
+        # collecting data
+            # datacontent = {'image': '', 'name': '', 'solditem': '', 'badge': '', 'price': '', 'promotion': '',
+            #                'shop': '', 'link': ''}
+            # soup = BeautifulSoup(response.text, 'html.parser')
+            # soup = soup.find('ul', {'class':'wt-grid wt-grid--block wt-pl-xs-0 tab-reorder-container'})
+            # soup = soup.findAll('li')
+            # for i in soup:
+            # #     if i.find('img') != None:
+            # #         datacontent['image'] = i.find('img')['src']
+            # #     else:
+            # #         None
+            #     if i.find('h3',{'class':'wt-text-caption v2-listing-card__title wt-text-truncate'}) != None:
+            #         datacontent['name'] = i.find('h3',{'class':'wt-text-caption v2-listing-card__title wt-text-truncate'}).text
+            #     else:
+            #         None
+            #     self.result['content'].append(datacontent)
+            # # for i in self.result['content']:
+            # print(self.result['content'])
 
 if __name__ == '__main__':
     result = WomenGift()
-    result.extract()
+    result.get_url('1')
+
 
 
