4da43b2ff9f045af7000737da697318feea180f9
HARITS <harits.muhammad.only@gmail.com>
2022-10-05 06:26:15 +0700

    get detail constructing

--- scrapetsy/__init__.py
+++ scrapetsy/__init__.py
@@ -2,26 +2,26 @@
 all function and method for scrapetsy
 """
 
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
-import time
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 class Scrapetsy:
     # define parent class variable
-    def __init__(self, scheme, host, filename, params, result):
+    def __init__(self, scheme, host, filename, params, description):
         self.scheme = scheme
         self.host = host
         self.params = params
         self.filename = filename
         self.header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'}
-        self.result = result
-    # define parent class function
+        self.description = description
 
-    # def get_page(self):
-    #     self.get_page()
+    # define parent class function
+    def get_page(self):
+        self.get_page()
 
     def get_url(self):
         self.get_url()
@@ -40,49 +40,101 @@ class WomenGift(Scrapetsy):
             host = 'www.etsy.com',
             filename = '/search', #?
             params = {'q':'gift+for+women', 'ref':'pagination', 'anchor_listing_id':'737271222', 'page':'1'},
-            result = {'title':'Most Popular Women Gift','description':'Class to get Most Popular Women Gift','content':['']})
+            description = {'title':'Most Popular Women Gift','description':'Class to get Most Popular Women Gift'})
 
     # define function in child class WomanGift
 
+    # Get URL Function
     def get_url(self):
         print('getting url...')
-        lastpage = 'Next'
+
+        # config webdriver
+        options = Options()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(executable_path='C:/geckodriver-v0.31.0-win64/geckodriver.exe', options=options)
+
+        # initial variable
         page = 1
-        while lastpage == 'Next':
-        # real access to web
-            try:
-                options = Options()
-                options.add_argument("--headless")
-                driver = webdriver.Firefox(executable_path='C:/geckodriver-v0.31.0-win64/geckodriver.exe', options=options)
-                url = f"{self.scheme}://{self.host}{self.filename}?q={self.params['q']}&ref={self.params['ref']}&anchor_listing_id={self.params['anchor_listing_id']}&page={str(page)}"
-                driver.get(url)
-                time.sleep(5)
-                response = driver.page_source
 
-            except:
-                response = 'Invalid Format'
+        #looping for page
+        # while 1:
+        #     try:
+        url = f"{self.scheme}://{self.host}{self.filename}?q={self.params['q']}&ref={self.params['ref']}&anchor_listing_id={self.params['anchor_listing_id']}&page={str(page)}"
+        driver.get(url)
 
-            soup = BeautifulSoup(response, 'html.parser')
+        # Wait for response until ID 'content' located
+        content = WebDriverWait(driver,10).until(ec.presence_of_element_located((By.ID,'content')))
+
+        # Selecting Element to get url
+        parent = content.find_element(By.XPATH,'/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul')
+        children = parent.find_elements(By.XPATH,'/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul/li')
+        i = 1
+        urls = []
+        for item in children:
+            url = item.find_element(By.XPATH,f'/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul/li[{i}]/div/div/a[1]').get_attribute('href')
+            urls.append(url)
+            i+=1
+        # except:
+        #     break
+
+
+        # Close driver
+        driver.quit()
+
+        print('getting url completed')
+
+        return urls
+
+    # Get Detail Function
+    def get_detail(self,urls):
+        print('getting details...')
+
+        # config webdriver
+        options = Options()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(executable_path='C:/geckodriver-v0.31.0-win64/geckodriver.exe', options=options)
+        result = []
+        data = {'image':'', 'title':'', 'price':'', 'outlet_name':'', 'link_outlet':'', 'item_sold':'', 'detail':[], 'description':'', 'reviews':'', 'url':''}
+        for i in urls:
+            driver.get(i)
+            content = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CSS_SELECTOR, '.wt-pt-xs-5')))
+            data['image'] = content.find_element(By.CSS_SELECTOR,'li.carousel-pane:nth-child(1) > img:nth-child(1)').get_attribute('src')
+            data['title'] = content.find_element(By.CSS_SELECTOR, 'h1.wt-text-body-03').text
+            data['price'] = content.find_element(By.CSS_SELECTOR, 'wt-text-title-03 > span:nth-child(2)').text
+            data['outlet_name'] = content.find_element(By.CSS_SELECTOR, '#listing-page-cart > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)').text
+            data['link_outlet'] = content.find_element(By.CSS_SELECTOR, '#listing-page-cart > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)').get_attribute('href')
+            data['item_sold'] = content.find_element(By.CSS_SELECTOR, '#listing-page-cart > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > span:nth-child(3)').text
+            details = content.find_element(By.CSS_SELECTOR, 'ul.wt-text-body-01')
+            details = details.find_elements(By.TAG_NAME, 'li')
+            for j in details:
+                data['detail'].append(j)
+            data['description'] = content.find_element(By.CSS_SELECTOR, 'p.wt-break-word').text
+            data['reviews'] = content.find_element(By.CSS_SELECTOR, 'h2.wt-mr-xs-2').text
+            data['url'] = i
+        result.append(data)
+
+        driver.quit()
+        print('getting details completed')
+
+        return result
 
         # f = open('response.html', 'w+', encoding="utf-8")
         # f.write(response)
         # f.close()
 
-        # checking file response.html
-        # soup = BeautifulSoup(open('response.html', encoding = 'utf-8'),'html.parser')
 
-            link_soup = soup.findAll('div', {'class':'js-merch-stash-check-listing v2-listing-card wt-mr-xs-0 search-listing-card--desktop listing-card-experimental-style appears-ready'})
-            for i in link_soup:
-                result = i.find('a', href=True)['href']
-                self.result['content'].append(result)
-
-            page_soup = soup.find('ul', {'class':'wt-action-group wt-list-inline search-pagination'})
-            page_soup = page_soup.findAll('span', {'class':'wt-screen-reader-only'})
-            lastpage = page_soup[-1].text
-
-            print(f'page {page} collected')
-            page += 1
-        return self.result['content']
+            # link_soup = soup.findAll('div', {'class':'js-merch-stash-check-listing v2-listing-card wt-mr-xs-0 search-listing-card--desktop listing-card-experimental-style appears-ready'})
+            # for i in link_soup:
+            #     result = i.find('a', href=True)['href']
+            #     self.result['content'].append(result)
+            #
+            # page_soup = soup.find('ul', {'class':'wt-action-group wt-list-inline search-pagination'})
+            # page_soup = page_soup.findAll('span', {'class':'wt-screen-reader-only'})
+            # lastpage = page_soup[-1].text
+            #
+            # print(f'page {page} collected')
+            # page += 1
+        # return self.result['content']
 
         # collecting data
             # datacontent = {'image': '', 'name': '', 'solditem': '', 'badge': '', 'price': '', 'promotion': '',
@@ -105,7 +157,10 @@ class WomenGift(Scrapetsy):
 
 if __name__ == '__main__':
     result = WomenGift()
-    print(len(result.get_url()))
+    urls = result.get_url()
+    result.get_detail(urls)
+    print(result)
+
 
 
 
