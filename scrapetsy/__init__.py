5484bec786941a4d505d18f43db4291d4d8e3416
HARITS <harits.muhammad.only@gmail.com>
2022-10-07 05:25:15 +0700

    to file constructing invalid output

--- scrapetsy/__init__.py
+++ scrapetsy/__init__.py
@@ -8,10 +8,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import json
+import csv
+
 
 class Scrapetsy:
     # define parent class variable
-    def __init__(self, scheme, host, filename, params, description, geckopaths):
+    def __init__(self, scheme, host, filename, params, description, webdriver_path):
         self.scheme = scheme
         self.host = host
         self.params = params
@@ -19,7 +21,7 @@ class Scrapetsy:
         self.header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'}
         self.description = description
-        self.geckopaths = geckopaths
+        self.webdriver_path = webdriver_path
 
     # define parent class function
     def get_page(self):
@@ -32,83 +34,112 @@ class Scrapetsy:
         self.to_csv()
 
 # Class to get popular women gift
+
+
 class WomenGift(Scrapetsy):
     # define variable in child class WomanGift
-    def __init__(self, geckopath):
+    def __init__(self, webdriver_path):
         super().__init__(
             # https://www.etsy.com/search?q=gift+for+women&ref=pagination&anchor_listing_id=737271222&page=1
-            scheme='https',  #://
+            scheme='https',
             host='www.etsy.com',
-            filename='/search',  # ?
+            filename='/search',
             params={'q': 'gift+for+women', 'ref': 'pagination', 'anchor_listing_id': '737271222', 'page': '1'},
             description={'title': 'Most Popular Women Gift', 'description': 'Class to get Most Popular Women Gift'},
-            geckopaths = geckopath
+            webdriver_path=webdriver_path
         )
     # define function in child class WomanGift
 
     # Get URL Function
-    def get_url(self):
+    def get_url(self, pagination=False):
         print('getting url...')
 
-        # config webdriver
-        options = Options()
-        options.add_argument("--headless")
-        driver = webdriver.Firefox(executable_path=self.geckopaths, options=options)
-
         # initial variable
+        url_list = []
         page = 1
-
-        #looping for page
-        # while 1:
-        #     try:
-        url = f"{self.scheme}://{self.host}{self.filename}?q={self.params['q']}&ref={self.params['ref']}&anchor_listing_id={self.params['anchor_listing_id']}&page={str(page)}"
-        driver.get(url)
-
-        # Wait for response until ID 'content' located
-        content = WebDriverWait(driver,10).until(ec.presence_of_element_located((By.ID,'content')))
-
-        # Selecting Element to get url
-        parent = content.find_element(By.XPATH,'/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul')
-        children = parent.find_elements(By.XPATH,'/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul/li')
         i = 1
-        urls = []
-        for item in children:
-            url = item.find_element(By.XPATH,f'/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul/li[{i}]/div/div/a[1]').get_attribute('href')
-            urls.append(url)
-            i+=1
-        # except:
-        #     break
+        if pagination is False:
+            # config webdriver
+            options = Options()
+            options.add_argument("--headless")
+            driver = webdriver.Firefox(executable_path=self.webdriver_path, options=options)
 
+            # looping for page
+            url = f"{self.scheme}://{self.host}{self.filename}?q={self.params['q']}&ref={self.params['ref']}&anchor_listing_id={self.params['anchor_listing_id']}&page={str(page)} "
+            driver.get(url)
 
-        # Close driver
-        driver.quit()
+            # Wait for response until ID 'content' located
+            content = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.ID, 'content')))
 
-        print('getting url completed')
+            # Selecting Element to get url
+            parent = content.find_element(By.XPATH, '/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul')
+            children = parent.find_elements(By.XPATH, '/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul/li')
+            for item in children:
+                url = item.find_element(By.XPATH, f'/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul/li[{i}]/div/div/a[1]').get_attribute('href')
+                url_list.append(url)
+                i += 1
 
-        return urls
+            # Close driver
+            driver.quit()
+
+            print('getting url completed')
+
+        if pagination is True:
+            # config webdriver
+            options = Options()
+            options.add_argument("--headless")
+            driver = webdriver.Firefox(executable_path=self.webdriver_path, options=options)
+
+            # looping for page
+            while 1:
+                try:
+                    url = f"{self.scheme}://{self.host}{self.filename}?q={self.params['q']}&ref={self.params['ref']}&anchor_listing_id={self.params['anchor_listing_id']}&page={str(page)}"
+                    driver.get(url)
+
+                    # Wait for response until ID 'content' located
+                    content = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.ID, 'content')))
+
+                    # Selecting Element to get url
+                    parent = content.find_element(By.XPATH, '/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul')
+                    children = parent.find_elements(By.XPATH, '/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul/li')
+                    for item in children:
+                        url = item.find_element(By.XPATH, f'/html/body/main/div/div[1]/div/div[3]/div[5]/div[4]/div[9]/div[1]/div/div/ul/li[{i}]/div/div/a[1]').get_attribute('href')
+                        url_list.append(url)
+                        i += 1
+                except Exception:
+                    break
+            # Close driver
+            driver.quit()
+
+            print('getting url completed')
+
+        return url_list
 
     # Get Detail Function
-    def get_detail(self,urls):
+    def get_detail(self, url_list):
         print('getting details...')
 
         # config webdriver
         options = Options()
         options.add_argument("--headless")
-        driver = webdriver.Firefox(executable_path=self.geckopaths, options=options)
+        driver = webdriver.Firefox(executable_path=self.webdriver_path, options=options)
+
+        # initial variable
+        data = {'image': '', 'title': '', 'price': '', 'outlet_name': '', 'link_outlet': '', 'item_sold': '', 'detail': [], 'description': '', 'reviews': '', 'url': ''}
         result = []
-        data = {'image':'', 'title':'', 'price':'', 'outlet_name':'', 'link_outlet':'', 'item_sold':'', 'detail':[], 'description':'', 'reviews':'', 'url':''}
-        for i in urls:
+        for i in url_list:
+            print(f'collecting {i}')
             driver.get(i)
             content = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.ID, 'content')))
-            data['image'] = content.find_element(By.CSS_SELECTOR,'li.carousel-pane:nth-child(1) > img:nth-child(1)').get_attribute('src')
+            data['image'] = content.find_element(By.CSS_SELECTOR, 'li.carousel-pane:nth-child(1) > img:nth-child(1)').get_attribute('src')
             data['title'] = content.find_element(By.CSS_SELECTOR, 'h1.wt-text-body-03').text
 
             # get price
-            prices1 = content.find_element(By.XPATH,"//p[@class='wt-text-title-03 wt-mr-xs-1']")
+            prices1 = content.find_element(By.XPATH, "//p[@class='wt-text-title-03 wt-mr-xs-1']")
             prices2 = prices1.find_elements(By.TAG_NAME, 'span')
             try:
                 data['price'] = prices2[-1].text
-            except:
+            except Exception:
                 data['price'] = prices1.text
             data['outlet_name'] = content.find_element(By.CSS_SELECTOR, '#listing-page-cart > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)').text
             data['link_outlet'] = content.find_element(By.CSS_SELECTOR, '#listing-page-cart > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)').get_attribute('href')
@@ -118,41 +149,60 @@ class WomenGift(Scrapetsy):
             details = content.find_element(By.CSS_SELECTOR, 'ul.wt-text-body-01')
             details = details.find_elements(By.TAG_NAME, 'li')
             for j in details:
-                data['detail'].append(j)
+                data['detail'].append(j.text)
             data['description'] = content.find_element(By.CSS_SELECTOR, 'p.wt-break-word').text
             data['reviews'] = content.find_element(By.CSS_SELECTOR, 'h2.wt-mr-xs-2').text
             data['url'] = i
-
-        result.append(data)
+            result.append(data)
         driver.quit()
         print('getting details completed')
 
         return result
 
-    def to_file(self,data,filepath):
+    def to_file(self, data, filepath):
         print('Creating file...')
         ext = filepath.split(".")[-1]
-        dir = filepath.rsplit("/",1)[0]
+        folder = filepath.rsplit("/", 1)[0]
         if ext == 'json':
             try:
-                os.mkdir(dir)
-            except:
+                os.mkdir(folder)
+            except Exception:
                 pass
-            f = open(filepath, 'w+', encoding="utf-8")
-            f.write(json.dump(data))
-            f.close()
+            with open(filepath, 'w+', encoding="utf-8", newline='') as f:
+                # for i in data:
+                json.dump(data, f)
+                f.close()
+
+        # to open file
+        # with open(filepath) as f:
+        #     data = json.load(f)
+        #     f.close()
+
+        elif ext == 'csv':
+            try:
+                os.mkdir(folder)
+            except Exception:
+                pass
+            with open(filepath, 'w+', encoding="utf-8", newline='') as f:
+                headers = ['image', 'title', 'price', 'outlet_name', 'link_outlet', 'item_sold', 'detail',
+                           'description', 'reviews', 'url']
+                writer = csv.DictWriter(f, fieldnames=headers)
+                writer.writeheader()
+                for i in data:
+                    writer.writerow(i)
+                f.close()
+
         else:
-            pass
+            print('Unknown format file')
         print(f'{filepath} created')
 
 
 if __name__ == '__main__':
-    scraper = WomenGift(geckopath = 'C:/geckodriver-v0.31.0-win64/geckodriver.exe')
+    scraper = WomenGift(webdriver_path='C:/geckodriver-v0.31.0-win64/geckodriver.exe')
     urls = scraper.get_url()
     result = scraper.get_detail(urls)
-    scraper.to_file(data = result, filepath='/result/result.json')
-
-
-
-
-
+    print(result)
+    # with open('C:/result/result.json') as f:
+    #     data = json.load(f)
+    #     f.close()
+    scraper.to_file(data=result, filepath='C:/project/ScrapingMarketplace/result/result.csv')
